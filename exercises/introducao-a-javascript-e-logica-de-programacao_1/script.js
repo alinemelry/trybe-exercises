acab9e92def280d820f769713db0ed2a808836dd
aline <aline.melryaa@gmail.com>
2021-03-01 13:49:07 -0400

    exercise_2/4.1

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/script.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/script.js
@@ -1,13 +1,11 @@
-let a = 5;
-let b = 36;
-let somaAdicao = (a + b);
-let subtracao= (a - b);
-let multiplicacao= (a * b);
-let divisao = (a / b);
-let modulo = (a % b);
-console.log("A Resultado é: ", + somaAdicao);
-console.log("A Resultado é: ", + subtracao);
-console.log("A Resultado é: ", + divisao);
-console.log("A Resultado é: ", + multiplicacao);
-console.log ("A Resultado é: ", + modulo);
 
+let c = 35;
+let d= 25;
+
+if(c < d ){
+    console.log("numero maior");
+
+}
+else{
+    console.log("mumero menor");
+}
